673f8ae7b0c284e0a5b9cc345bac9e4e13339ab4
sasa <sasakuliah@gmail.com>
2026-05-29 22:04:48 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,37 @@
 import { Routes, Route } from 'react-router-dom'
 
-// Import semua cangkang halaman yang sudah kita buat sebelumnya
+// Import Halaman
 import LandingPage from './pages/LandingPage'
 import AuthPage from './pages/AuthPage'
 import Dashboard from './pages/Dashboard'
 import PredictPage from './pages/PredictPage'
+import ProfilePage from './pages/ProfilePage'
 import NotFound from './pages/NotFound'
+
+// Import Layouts
+import ProtectedLayout from './layouts/ProtectedLayout'
+import PublicLayout from './layouts/PublicLayout'
 
 export default function App() {
   return (
     <Routes>
-      {/* Rute Publik */}
+      {/* Rute Bebas Murni (Landing Page tetap bisa diakses siapa saja) */}
       <Route path="/" element={<LandingPage />} />
-      <Route path="/login" element={<AuthPage />} />
-      <Route path="/register" element={<AuthPage />} />
 
-      {/* Rute Aplikasi Utama */}
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/predict" element={<PredictPage />} />
+      {/* Rute Publik Terbatas (Hanya untuk yang BELUM login) */}
+      <Route element={<PublicLayout />}>
+        <Route path="/login" element={<AuthPage />} />
+        <Route path="/register" element={<AuthPage />} />
+      </Route>
 
-      {/* Rute 404 - Akan menangkap URL acak yang tidak terdaftar di atas */}
+      {/* Rute Terkunci (Hanya untuk yang SUDAH login) */}
+      <Route element={<ProtectedLayout />}>
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/predict" element={<PredictPage />} />
+        <Route path="/profile" element={<ProfilePage />} /> {/* Tambahan rute profil di sini */}
+      </Route>
+
+      {/* Penangkap URL Nyasar */}
       <Route path="*" element={<NotFound />} />
     </Routes>
   )
