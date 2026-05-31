1b205ed11a0a13e5a69a5adc740db1bc6b9caa61
sasa <sasakuliah@gmail.com>
2026-05-31 09:52:10 +0700

    Update ActionCard.jsx

--- src/components/dashboard/ActionCard.jsx
+++ src/components/dashboard/ActionCard.jsx
@@ -2,16 +2,19 @@ import { Link } from 'react-router-dom'
 
 export default function ActionCard() {
   return (
-    <div className="bg-slate-200 rounded-3xl p-8 h-64 flex flex-col items-center justify-center text-center shadow-sm">
-      <h3 className="text-xl font-bold text-slate-800 mb-2">
+    <div className="bg-slate-200 dark:bg-slate-800 rounded-3xl p-8 h-64 flex flex-col items-center justify-center text-center shadow-sm dark:shadow-none transition-colors duration-300">
+      
+      <h3 className="text-xl font-bold text-slate-800 dark:text-white mb-2 transition-colors">
         Bagikan Aktivitasmu!
       </h3>
-      <p className="text-xs text-slate-500 max-w-52">
+      
+      <p className="text-xs text-slate-500 dark:text-slate-400 max-w-52 transition-colors">
         Catat jam tidur, stres, dan produktivitasmu hari ini.
       </p>
+      
       <Link 
         to="/predict"
-        className="mt-6 px-10 py-3 bg-slate-500 text-white font-bold rounded-xl hover:bg-slate-600 transition-all shadow-md hover:shadow-lg"
+        className="mt-6 px-10 py-3 bg-slate-500 dark:bg-slate-600 text-white font-bold rounded-xl hover:bg-slate-600 dark:hover:bg-slate-500 transition-all shadow-md dark:shadow-none hover:shadow-lg hover:-translate-y-1"
       >
         Catat
       </Link>
