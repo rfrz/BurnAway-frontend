1fec1100a18adead93cd9dee7190fcd3ba64fa42
sasa <sasakuliah@gmail.com>
2026-05-31 09:57:19 +0700

    Update AuthPage.jsx
    sinkronisasi warna pada tema gelap

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -7,28 +7,30 @@ export default function AuthPage() {
   const [activeTab, setActiveTab] = useState('login')
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-slate-50 p-4">
-      {/* Kotak Form (Card) */}
-      <div className="w-full max-w-md bg-white rounded-2xl shadow-xl p-8">
+    <div className="min-h-screen flex items-center justify-center bg-slate-50 dark:bg-[#0f172a] p-4 transition-colors duration-300">
+      
+      {/* 2. Kotak Form (Card) */}
+      <div className="w-full max-w-md bg-white dark:bg-slate-800 rounded-2xl shadow-xl dark:shadow-none border border-transparent dark:border-slate-700 p-8 transition-colors duration-300">
         
-        {/* Tombol Tab (Toggle) */}
-        <div className="flex bg-slate-100 p-1 rounded-xl mb-6">
+        {/* 3. Latar Tab (Toggle)*/ }
+        <div className="flex bg-slate-100 dark:bg-slate-900/50 p-1 rounded-xl mb-6 transition-colors">
           <button
             onClick={() => setActiveTab('login')}
             className={`w-1/2 py-2 text-center rounded-lg font-bold transition-all ${
               activeTab === 'login' 
-                ? 'bg-white shadow-sm text-slate-800' 
-                : 'text-slate-500 hover:text-slate-700'
+                ? 'bg-white dark:bg-slate-700 shadow-sm text-slate-800 dark:text-white' 
+                : 'text-slate-500 dark:text-slate-400 hover:text-slate-700 dark:hover:text-slate-200'
             }`}
           >
             Masuk
           </button>
+          
           <button
             onClick={() => setActiveTab('register')}
             className={`w-1/2 py-2 text-center rounded-lg font-bold transition-all ${
               activeTab === 'register' 
-                ? 'bg-white shadow-sm text-slate-800' 
-                : 'text-slate-500 hover:text-slate-700'
+                ? 'bg-white dark:bg-slate-700 shadow-sm text-slate-800 dark:text-white' 
+                : 'text-slate-500 dark:text-slate-400 hover:text-slate-700 dark:hover:text-slate-200'
             }`}
           >
             Daftar
